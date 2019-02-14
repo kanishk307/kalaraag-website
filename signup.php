513fd86c6916950a8289ce8f70a065889aaf2014
kanishk307 <ysj30797.kj@gmail.com>
2019-02-14 22:58:59 +0530

    signup

--- signup.php
+++ signup.php
@@ -1,3 +1,58 @@
+ <?php
+$msg = '';
+$msgClass= '';
+if(filter_has_var(INPUT_POST, 'submit')){
+//     echo "submitted";
+//    get form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    
+    if(!empty($name) && !empty($email) && !empty($message)){
+        //passed
+        //checking for email
+        if(filter_var($email,FILTER_VALIDATE_EMAIL)==false){
+            $msg = 'Please fill all fields correctly';
+            $msgClass = 'alert-danger';
+        } else{
+//             echo 'Passed';
+               //Recipient email
+               $toEmail = "ysj30797.kj@gmail.com";
+               $subject = "Contact request from $name";
+               $body = "<h2>Contact Request</h2>
+               <h4>Name : $name</h4>
+                <h4>Email : $email</h4> 
+                 <h4>Message : $message</h4>";
+               
+               // Setting email headers here
+               $headers = "MIME-Version: 1.0" ."\r\n";
+               $headers .="Content-Type:text/html;charset=UTF-8" . "\r\n";
+               
+               // Additional Headers
+               $headers .= "From: " .$name. "<".$email.">". "\r\n";
+        } 
+     
+         
+         
+    if(mail($toEmail, $subject, $body,$headers)){
+        $msg = 'Thank you for sending your details. We will respond soon.';
+        $msgClass = 'alert-success';
+    }else{
+        $msg = 'You email was not sent';
+        $msgClass = 'alert-danger';
+    }
+    }
+    else{
+        //failed
+        $msg = 'Please fill all fields correctly';
+        $msgClass = 'alert-danger';
+    }
+    
+    
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -64,7 +119,39 @@
 </nav>
    
    
+<section id="signup-first-box">
+<!--    <div class="container">-->
+        <div class="container" style="margin: 50px auto; width:30%; ">
+          <?php if($msg != ''):?>
+          	<div class="alert <?php echo $msgClass;?>"><?php echo $msg;?></div>
+          <?php endif;?>
+            <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" class="form-control" value="<?php echo isset($name)? $name : ''; ?>">
+                    
+                </div>
 
+                <div class="form-group">
+                    <label for="email">Email  </label>
+                    <input type="text" name="email" class="form-control"  value="<?php echo isset($email)? $email : ''; ?>">
+                </div>
+
+                <div class="form-group">
+                    <label for="message">Message</label>
+                   <textarea name="message"  cols="30" rows="10" class="form-control"><?php echo isset($message)? $message : ''; ?></textarea>
+                </div>
+
+                <div class="form-group">
+                  <button type="submit" name="submit" class="btn btn-success">Submit</button>
+                </div>
+
+            </form>
+        </div>  
+
+        
+    </div>
+</section>
 
 
 
